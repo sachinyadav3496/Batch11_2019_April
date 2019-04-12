758cd7785055676a5195c8ae5956075555d981a0
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-04-12 13:21:56 +0530

    added code for data storage in files

--- web_design/flask_applications/project4/app.py
+++ web_design/flask_applications/project4/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template,request
-import os 
+import os,json
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+USER_DATA = os.path.join(BASE_DIR,"static\\data")
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -12,11 +14,13 @@ def login():
         email = request.form['email']
         password = request.form['password']
         if len(email) > 5 and len(password) > 8 : 
-
-            data = {
-                'email' : email ,
-                'Password' :  password }
-            return render_template("profile.html",title="profile",info=data,flag=True)
+            tmp_path = "users\\"+email.strip()+".json"
+            path = os.path.join(USER_DATA,tmp_path)
+            if os.path.exists(path) : 
+                return "Sucess"
+            else :
+                error = "No such User Exists, Please Signup First" 
+                return render_template("index.html",title="home",error=error)
         else : 
             error = "Length of UserName and Password Should Be greater."
     else : 
@@ -30,17 +34,32 @@ def signup():
 @app.route("/mksignup/",methods=["GET","POST"])
 def mksignup():
     if request.method == "POST" : 
-        data = { 
-            'User Name' : request.form['username'],
-            'First Name' : request.form['fname'],
-            'Last Name' : request.form['lname'],
-            'Email' : request.form['email'],
-            'Phone Number' : request.form['ph_no'],
-            'Password' : request.form['password'],
-            'img_path' : ''
-        }
-
-        return render_template("mksignup.html",data=data,title="Profile")
+        email = request.form['email']
+        new_path = "users\\"+email.strip()+ ".json"
+        path = os.path.join(USER_DATA,new_path)
+        if os.path.exists(path):
+            error = "User Already Exists Please Signin to Enjoy Your Services"
+            return render_template("index.html",error=error,title='home')
+        else : 
+            file = request.files['pic']
+            #if file.filename.split('.')[-1] in [ 'jpg','jpeg','png',]
+            tmp_path = "users_profile\\"+email.strip()+".jpg"
+            filename = os.path.join(USER_DATA,tmp_path)
+            file.save(filename)
+            data = { 
+                'User Name' : request.form['username'],
+                'First Name' : request.form['fname'],
+                'Last Name' : request.form['lname'],
+                'Email' : request.form['email'],
+                'Phone Number' : request.form['ph_no'],
+                'Password' : request.form['password'],
+                'img_path' : filename,
+            }
+            f = open(path,'w')
+            json.dump(data,f)
+            f.close()
+            error = "Account Successfully Created Please Login Now"
+            return render_template("index.html",title="Profile",error=error)
     else : 
         error = "Method Not Allowed"
         return render_template("signup.html",error=error)
