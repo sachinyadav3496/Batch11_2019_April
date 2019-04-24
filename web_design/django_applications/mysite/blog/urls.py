0ac61c7ae8d308bb43726457c8529c9af8ee98fc
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-04-24 09:19:29 +0530

    added code for urls using variables to retrieve query from user

--- web_design/django_applications/mysite/blog/urls.py
+++ web_design/django_applications/mysite/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
 from blog import views 
 urlpatterns = [ 
-
     path('',views.index,name='blog'),
     path('data/',views.data),
+    path('data/<str:name>/',views.data),
+    path('pages/<int:year>/',views.page_year_view),
+    path('pages/<int:year>/<int:month>/',views.page_month_view),
+    path('pages/<int:year>/<int:month>/<int:day>/',views.page_day_view),
 ]
--- web_design/django_applications/mysite/blog/views.py
+++ web_design/django_applications/mysite/blog/views.py
@@ -5,5 +5,14 @@ def index(request):
 
     return render(request,'blog/index.html')
 
-def data(request):
-    return HttpResponse("Welcome to data")
+def data(request,name='Django'):
+    return HttpResponse(f"Welcome to data mr. {name}")
+
+def page_year_view(reqeust,year):
+    return HttpResponse(f"you are seeking pages for the year {year}")
+
+def page_month_view(request,year,month):
+    return HttpResponse(f"You are seeeking pages for the month {month} of year {year}")
+
+def page_day_view(request,year,month,day):
+    return HttpResponse(f"You are seeking specific to date {year}/{month}/{day}")
